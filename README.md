542bf2ffe64b4be8ba5902ce4e908ee462f84c5d
Michael Fisher <mifi@hey.com>
2024-05-19 21:32:46 +0300

    LED blinky example

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn ESP32
+
+This repo is the trace of me playing with ESP32-S3 and passing excecises from [the Freenove Ultimate Starter Kit](https://github.com/Freenove/Freenove_Ultimate_Starter_Kit_for_ESP32_S3).
---
+++ examples/01-led/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{clock::ClockControl, delay::Delay, gpio::IO, peripherals::Peripherals, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let delay = Delay::new(&clocks);
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = io.pins.gpio2.into_push_pull_output();
+
+    esp_println::logger::init_logger_from_env();
+
+    loop {
+        log::info!("Hello world!");
+        led.set_high();
+        delay.delay(500.millis());
+        led.set_low();
+        delay.delay(500.millis());
+    }
+}
